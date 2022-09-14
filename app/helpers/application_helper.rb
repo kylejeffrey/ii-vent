cb755d1602a0008d55a07b56834d676c089a2dd5
Kyle Jeffrey <kyle.m.jeffrey@gmail.com>
2022-09-14 12:30:45 +0900

    can now access an event as a user with no avatar_url

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,11 +7,21 @@ module ApplicationHelper
     params[:controller] == 'devise/sessions' && params[:action] == 'new'
   end
 
-  def avatar?
+  def current_user_avatar?
     if current_user.photo.attached?
       current_user.photo
     elsif current_user.avatar_url?
       current_user.avatar_url
+    else
+      "https://pdtxar.com/wp-content/uploads/2019/04/person-placeholder.jpg"
+    end
+  end
+
+  def event_member_avatar?
+    if event_member.user.avatar_url?
+      event_member.user.avatar_url
+    # elsif event_member.user.photo.attached?
+    #   event_member.user.photo
     else
       "https://pdtxar.com/wp-content/uploads/2019/04/person-placeholder.jpg"
     end
