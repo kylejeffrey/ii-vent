a533044e306c15472a0ada44cb9a40fc0b7d6446
Kyle Jeffrey <kyle.m.jeffrey@gmail.com>
2022-09-14 11:54:35 +0900

    added avatar? helper to help display navbar - working on new member form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,14 @@ module ApplicationHelper
   def signin_page?
     params[:controller] == 'devise/sessions' && params[:action] == 'new'
   end
+
+  def avatar?
+    if current_user.photo.attached?
+      current_user.photo
+    elsif current_user.avatar_url?
+      current_user.avatar_url
+    else
+      "https://pdtxar.com/wp-content/uploads/2019/04/person-placeholder.jpg"
+    end
+  end
 end
